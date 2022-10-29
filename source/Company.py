e4cbae9d3cc7836376452cc119d376174a46a857
YuvalRi <yuvalritsker@gmail.com>
2022-10-29 20:05:34 +0300

    all function until __repr__ function

--- source/Company.py
+++ source/Company.py
@@ -1,53 +1,91 @@
+def check_string(str: str):
+    '''
+    A function which checks the length of given str
+    (should be at least 2 letters) and checks if the
+    first letter is a capital letter.
+    '''
+    if not str[0].isupper() or len(str) >= 2:
+        return False
+    return True
+
+
+def check_int(num: int):
+    '''
+    A function which returns True if the given number
+    is positive and False otherwise
+    '''
+    if num <= 0:
+        return False
+    return True
+
 
 class Company:
     # you can add methods for your usage
 
     def __init__(self, name: str, stocks_num: int,
                  stock_price: int, comp_type: str):
-        if not name[0].isupper or len(name) < 2:
+        if not check_string(name):
             raise ValueError("Error! Company name starts with capital letter.")
         else:
-            self.set_name = name
-        if stocks_num <= 0:
+            self.name = name
+        if not check_int(stocks_num):
             raise ValueError("Error! Number of stocks must be positive.")
         else:
-            self.set_stocks_num = stocks_num
-        if stock_price <= 0:
+            self.stocks_num = stocks_num
+        if not check_int(stock_price):
             raise ValueError("Error! Price of stock must be positive.")
         else:
-            self.set_stock_price = stock_price
-        if not comp_type[0].isupper or len(comp_type) < 2:
+            self.stock_price = stock_price
+        if not check_string(comp_type):
             raise ValueError("Error! Company type starts with capital letter.")
         else:
-            self.set_comp_type = comp_type
+            self.comp_type = comp_type
 
     def net_worth(self):
-
-        pass
+        return self.stock_price*self.stocks_num
 
     def set_name(self, name):
-
-        pass
+        if not check_string(name) or self.name == name:
+            return False
+        else:
+            name = self.name
+            return True
 
     def set_stocks_num(self, stocks_num):
-
-        pass
+        if not stocks_num != self.stocks_num or check_int(stocks_num):
+            return False
+        else:
+            stocks_num = self.stocks_num
+            return True
 
     def set_stock_price(self, stock_price):
-
-        pass
+        if not check_int(stock_price) or stock_price < self.net_worth():
+            return False
+        else:
+            stock_price == self.stock_price
+            return True
 
     def set_comp_type(self, comp_type):
-
-        pass
+        if not check_string(comp_type) or comp_type == self.comp_type:
+            return False
+        else:
+            comp_type = self.set_comp_type
+        return True
 
     def update_net_worth(self, net_worth):
-
-        pass
-
+        if not check_int(net_worth) or net_worth != self.net_worth():
+            return False
+        else:
+            self.set_stock_price = net_worth/self.set_stocks_num
+            return True
+ 
     def add_stocks(self, number):
-
-        pass
+        new_stock_num = self.set_stocks_num + number
+        if new_stock_num <= 0:
+            return False
+        else:
+            self.set_stocks_num = new_stock_num
+            return True
 
     def __repr__(self):
 
