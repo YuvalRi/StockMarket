7dfb943fc9dc60afd5e60559000f1945f48c11c5
YuvalRi <yuvalritsker@gmail.com>
2022-10-29 16:23:07 +0300

    construction function

--- source/Company.py
+++ source/Company.py
@@ -2,8 +2,24 @@
 class Company:
     # you can add methods for your usage
 
-    def __init__(self, name, stocks_num, stock_price, comp_type):
-        pass
+    def __init__(self, name: str, stocks_num: int,
+                 stock_price: int, comp_type: str):
+        if not name[0].isupper or len(name) < 2:
+            raise ValueError("Error! Company name starts with capital letter.")
+        else:
+            self.set_name = name
+        if stocks_num <= 0:
+            raise ValueError("Error! Number of stocks must be positive.")
+        else:
+            self.set_stocks_num = stocks_num
+        if stock_price <= 0:
+            raise ValueError("Error! Price of stock must be positive.")
+        else:
+            self.set_stock_price = stock_price
+        if not comp_type[0].isupper or len(comp_type) < 2:
+            raise ValueError("Error! Company type starts with capital letter.")
+        else:
+            self.set_comp_type = comp_type
 
     def net_worth(self):
 
