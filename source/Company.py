a1d241a66939215b29f4fac5a8db0e67f4f079df
YuvalRi <yuvalritsker@gmail.com>
2022-11-11 15:14:34 +0200

    stockmarket class functions Co-authored-by: ofekshmu <ofekshmu@users.noreply.github.com>

--- source/Company.py
+++ source/Company.py
@@ -34,118 +34,74 @@ class Company:
     # The constructor function
     def __init__(self, name: str, stocks_num: int,
                  stock_price: float, comp_type: str):
-        if not valid_name(name):
-            raise ValueError("Error! Company name starts with capital letter.")
-        else:
-            self.name = name
-        if stocks_num < 0 or check_int(stocks_num) is False:
-            raise ValueError("Error! Number of stocks must be positive.")
-        else:
-            self.stocks_num = stocks_num
-        if stock_price < 0:
-            raise ValueError("Error! Price of stock must be positive.")
-        else:
-            self.stock_price = stock_price
-        if not valid_name(comp_type):
-            raise ValueError("Error! Company type starts with capital letter.")
-        else:
-            self.comp_type = comp_type
+        pass
 
     def net_worth(self):
         '''
         Net worth of a company: as simple as that
         '''
-        return self.stock_price*self.stocks_num
+        pass
 
     def set_name(self, name):
         '''
         Updates the company name, as long as its valid
         '''
-        if name == self.name or valid_name(name) is False:
-            return False
-        self.name = name
-        return True
+        pass
 
     def set_stocks_num(self, stocks_num):
         '''
         Updates the number of stocks, as long as its valid
         '''
-        if stocks_num < 0 or check_int(stocks_num) is False:
-            return False
-        net_worth = self.net_worth()
-        self.stocks_num = stocks_num
-        self.stock_price = net_worth/self.stocks_num
-        return True
+        pass
 
     def set_stock_price(self, stock_price):
         '''
         Updates the price of a stock, as long as its valid
         '''
-        if stock_price > self.net_worth() or stock_price < 0:
-            return False
-        else:
-            net_worth = self.net_worth()
-            self.stock_price = stock_price
-            self.stocks_num = int(net_worth/self.stock_price)
-            return True
+        pass
 
     def set_comp_type(self, comp_type):
         '''
         Updates the type of the copmany, as long as its valid
         '''
-        if valid_name(comp_type):
-            self.comp_type = comp_type
-            return True
-        return False
+        pass
 
     def update_net_worth(self, net_worth):
         '''
         Updates the networth of the company
         '''
-        if net_worth > 0:
-            self.stock_price = net_worth / self.stocks_num
-            return True
-        return False
+        pass
 
     def add_stocks(self, number):
         '''
         The function get int (numeber) and add it
         to the current number of stocks
         '''
-        new_stock_num = self.stocks_num + number
-        if check_int(number) is False or new_stock_num <= 0:
-            return False
-        else:
-            self.stocks_num = new_stock_num
-            return True
+        pass
 
     def __str__(self):
         '''
         A string with all the required data of the company
         '''
-        return f"{self.name}, {self.stocks_num} stocks, Price: {self.stock_price}, {self.comp_type}, Net Worth: {self.net_worth()}"
+        pass
 
     def __repr__(self):
         '''
         A string with all the required data of the company
         '''
-        return f"{self.name}, {self.stocks_num} stocks, Price: {self.stock_price}, {self.comp_type}, Net Worth: {self.net_worth()}"
+        pass
 
     def __lt__(self, other):
         '''
         Operator overloading - less than
         '''
-        if self.net_worth() < other.net_worth():
-            return True
-        return False
+        pass
 
     def __gt__(self, other):
         '''
         Operator overloading - greater than
         '''
-        if self.net_worth() > other.net_worth():
-            return True
-        return False
+        pass
 
     def __eq__(self, other):
         '''
@@ -159,9 +115,4 @@ class Company:
         '''
         Operator overloading which merges two companies into one
         '''
-        stocks_num_both = self.stocks_num + other.stocks_num
-        new_company = Company(name=self.name,
-                              stocks_num=self.stocks_num + other.stocks_num,
-                              stock_price=(self.net_worth() + other.net_worth())/stocks_num_both,
-                              comp_type=self.comp_type)
-        return new_company
+        pass
--- source/StockMarket.py
+++ source/StockMarket.py
@@ -1,12 +1,11 @@
-from Company import Company
-from Company import valid_name
+from Company import Company, valid_name
 
 # Stock Market Class exercise
 
 
 def unique_name(lst: list):
     '''
-    A function which checks if the strings
+    A function which checks if the elements
     in the list are unique
     '''
     if len(set(lst)) == len(lst):
@@ -24,16 +23,96 @@ class StockMarket:
                  companies=[]):
         if not valid_name(name):
             raise ValueError("Error! Please enter a valid name.")
+        else:
+            self.name = name
         if min_net_worth_threshold < 0:
             raise ValueError("Error! Please enter an unnegative number")
+        else:
+            self.min_net_worth_threshold = min_net_worth_threshold
         for ele in stock_market_types:
             if not valid_name(ele):
                 raise ValueError("Error! Each element should be valid")
+        else:
+            self.stock_market_types = stock_market_types
         if not unique_name(companies):
             raise ValueError("Error! Each company has a unique name.")
-    
+        else:
+            self.companies = companies
+
     def market_net_worth(self, comp_type: str):
-        if StockMarket.
+        '''
+        A function which returns the total net worth
+        of all the companies of comp_type
+        '''
+        total = 0
+        for comp in self.companies:
+            if comp.comp_type == comp_type:
+                total += comp.net_worth()
+        return total
+
+    def insert(self, c):
+        '''
+        A function adding a company (c) to the list
+        of companies (companies)
+        '''
+        if c in self.companies or self.min_net_worth_threshold <= 0 or type(c) != Company:
+            return False
+        else:
+            self.companies.append(c)
+            return True
+
+    def top_n(self, n: int):
+        '''
+        A function which returns net worth sorted list of
+        the required companies from the biggest to the lowest
+        '''
+        if len(self.companies) == 0:
+            return self.companies
+        else:
+            sorted_companies = self.companies.sort(reverse=True)
+            if n > len(self.companies):
+                return sorted_companies
+            return sorted_companies[:n]
+
+    def merger(self, c1, c2):
+        '''
+        A function which merges between 2 companies
+        according to the required conditions
+        '''
+        if c1 in self.companies and c2 in self.companies:
+            comp_1 = Company(name=c1,
+                             stocks_num=c1.stocks_num,
+                             stock_price=c1.stock_price,
+                             comp_type=c1.comp_type)
+            comp_2 = Company(name=c2,
+                             stocks_num=c2.stocks_num,
+                             stock_price=c2.stock_price,
+                             comp_type=c2.comp_type)
+            new_comp = comp_1 + comp_2
+            if comp_1 == comp_2:
+                new_comp.name = comp_1.name
+            if comp_1 > comp_2:
+                new_comp.name = comp_1.name
+            else:
+                new_comp.name = comp_2.name
+            self.companies.append(new_comp)
+            self.companies.clear(comp_1)
+            self.companies.clear(comp_2)
+            return new_comp
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
